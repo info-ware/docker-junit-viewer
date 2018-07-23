f277ee96e517b37517319d4982d51178164c4235
Simon Resch <resch@infoware.de>
2018-07-23 16:10:49 +0200

    Express server for host html.

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,9 @@ RUN npm install junit-viewer -g
 
 EXPOSE 8080
 
+ADD package.json .
+ADD test_converter.js .
+
+RUN npm install
+
 #RUN junit-viewer --results=/test_results --port=8080
---
+++ test_converter.js
@@ -0,0 +1,28 @@
+var express = require('express')
+const { exec } = require('child_process');
+var app = express()
+
+var jv = require('junit-viewer')
+//var parsedData = jv.parseXML('test.xml')
+//var renderedData = jv.render(parsedData)
+
+const testPath = '/routing_server_tests/'
+
+app.get('/', function (req, res) {
+    const piplineNum = req.param('PIPE');
+
+    exec(`mkdir ${testPath}${piplineNum}`)
+    exec(`junit-viewer --results=${testPath}routing_server_test_${piplineNum}.xml --save=${testPath}${piplineNum}/routing_server_test.html`, (err, stdout, stderr) => {
+    if (err) {
+        // node couldn't execute the command
+        return;
+    }
+
+    // the *entire* stdout and stderr (buffered)
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    res.sendFile(`${testPath}${piplineNum}/routing_server_test.html`)
+    });
+})
+ 
+app.listen(8080)

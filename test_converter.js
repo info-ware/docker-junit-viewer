21ea9d58791c4ff7f17742867718486c93ce009a
Simon Resch <resch@infoware.de>
2018-08-27 09:22:56 +0200

    Added API Internal unittest support.

--- test_converter.js
+++ test_converter.js
@@ -3,25 +3,35 @@ const { exec } = require('child_process');
 var app = express()
 
 var jv = require('junit-viewer')
-//var parsedData = jv.parseXML('test.xml')
-//var renderedData = jv.render(parsedData)
-
-const testPath = '/routing_server_tests/'
 
 app.get('/', function (req, res) {
     const piplineNum = req.param('PIPELINE');
+    const piplineJob = req.param('TEST_TYPE')
 
-    exec(`mkdir ${testPath}${piplineNum}`)
-    exec(`junit-viewer --results=${testPath}routing_server_test_${piplineNum}.xml --save=${testPath}${piplineNum}/routing_server_test.html`, (err, stdout, stderr) => {
-    if (err) {
-        // node couldn't execute the command
-        return;
+    var testPath = ''
+    var testName = ''
+    if (piplineJob == 'API') 
+    {
+        testPath = '/api_tests/'
+        testName = 'api_test'
+    }
+    else
+    {
+        testPath = '/routing_server_tests/'
+        testName = 'routing_server_test'
     }
 
-    // the *entire* stdout and stderr (buffered)
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-    res.sendFile(`${testPath}${piplineNum}/routing_server_test.html`)
+    exec(`mkdir ${testPath}${piplineNum}`)
+    exec(`junit-viewer --results=${testPath}${testName}_${piplineNum}.xml --save=${testPath}${piplineNum}/${testName}.html`, (err, stdout, stderr) => {
+        if (err) {
+            // node couldn't execute the command
+            return;
+        }
+
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        res.sendFile(`${testPath}${piplineNum}/${testName}.html`)
     });
 })
  
